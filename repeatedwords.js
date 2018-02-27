9595e4a11e39ff978ee8a927ce5207d42b78058f
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2018-02-27 08:42:56 +0000

    README.md

--- repeatedwords.js
+++ repeatedwords.js
@@ -1,15 +1,32 @@
 const fs = require('fs');
+const XRegExp = require('xregexp');
 const chalk = require('chalk');
-
+const xreg = XRegExp(`
+          \\b                      # frontera de palabra
+          ([a-z]+)                 # palabra
+          (
+            (\\s|<[^>]+>)+         # blancos o HTML tags
+            \\1                    # la palabra primera de nuevo
+          )+
+          \\b                      # frontera de palabra
+      `, 
+      'xgi'   // opciones: espacios global ignore-case
+);
 const ARGV = process.argv.slice(2);
 for (let file of ARGV) {
   let data = fs.readFileSync(file, {encoding: 'utf8'});
   let count = 0;
-  let highlight = data.replace(/\b([a-z]+)((\s|<[^>]+>)+\1)+\b/ig, chalk.blue("$&"));
-  let h = highlight.replace(/^|\n(?!$)/g, (x) => { 
+  let highlight = data.replace(xreg, chalk.underline("$&"));
+  let h = highlight.replace(
+    XRegExp(`
+      ^           # o bien es el comienzo
+      |
+      \\n         # o bien es un retorno de carro
+      (?!$)       # que no es el último
+    `, 'xg'),   // ignorar espacios, global
+    (x) => { 
       ++count; 
-      return `\n${file}(${count}): `; 
+      return `\n${chalk.bold(file)}(${count}): `; 
   });
   console.log(h);
 }
- 
